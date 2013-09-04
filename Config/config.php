a0def1393b06818f5fe3fe27510b888a7f555da9
Fernando Andrade <capetafromhell@jesusvoltara.zzn.com>
2013-09-04 20:55:52 -0300

    Algumas funcoes do Model adicionadas

--- Config/config.php
+++ Config/config.php
@@ -1,2 +1,21 @@
 <?php
+
+class Config {
+	
+	/* Database Config  */
+	var $type = "MySQL"; // Only MySQL
+	var $host = "localhost";
+	var $user = "root";
+	var $password = "1234";
+	var $db = "";
+	var $login_table = "usuarios";
+	var $books_table = "bookmarks";
+	
+	
+	/* General Config */
+	var $not_found = "404.html";
+	var $forbidden = "403.html";
+	
+}
+
 ?>
--- Model/db.php
+++ Model/db.php
@@ -1,2 +1,61 @@
 <?php
+
+include_once 'htdocs/BookmarksPHP/Config/config.php';
+
+class Model extends Config {
+	
+	var $handle;
+	
+	function Connect() {
+		if(!$this->handle) {
+		switch($this->type) {
+			case "MySQL":
+				$this->handle = mysql_pconnect($this->host,$this->user,$this->password);
+				if(!$this->handle) {
+					die("Error ao conectar ao servidor<br/>".mysql_error($this->handle));
+				}
+			break;
+		}
+	}
+	}
+	
+	function Disconnect() {
+		switch ($this->type) {
+			case "MySQL":
+				mysql_close($this->handle);
+				break;
+		}
+	}
+	
+	function __construct() {
+		$this->Connect();
+	}
+	
+	function __destruct() {
+		$this->Disconnect();
+	}
+
+	function Query($query) {
+		$this->Connect();
+		return mysql_query($query, $this->handle);
+	}
+
+	function Login($usuario, $senha) {
+		$this->Connect();
+		$consulta = $this->Query("SELECT * FROM ".$this->login_table." WHERE user='".mysql_real_escape_string($usuario)."'AND password='".mysql_real_escape_string($senha)."'");
+		if(mysql_num_rows($consulta) == 1) {
+			$resultado = mysql_fetch_assoc($consulta);
+			$retorno = Array("Existe" => true,
+							"id" => $resultado['id'],
+							"nome" => $resultado['nome'],
+							"email" => $resultado['email']);
+		} else {
+			$retorno = Array("Existe" => false);
+		}
+		
+		return $retorno;
+	}
+
+}
+
 ?>
